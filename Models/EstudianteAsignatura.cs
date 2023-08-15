1d8715214a7844df507204fad09357173473e450
Emmanuel Camacho <emmanuel.camachod@gmail.com>
2023-08-14 21:32:47 -0400

    Update all models without usuario,permisos,perfiles

---
+++ Models/EstudianteAsignatura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class EstudianteAsignatura
+    {
+        [Key]
+        public int Id { get; set; }
+        public int EstudianteId { get; set; }
+        public int AsignaturaId { get;set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public DateTime FechaActualizacion { get; set; } = DateTime.Now;
+        public int Estado { get; set; } = 1;
+    }
+}
---
+++ Models/Profesor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Profesor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CodigoProfesor { get; set; }
+        public string Identificacion { get; set; } 
+        public string Nombre { get; set; }
+        public string Apellido { get; set; } 
+        public int Nacionalidad { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public DateTime FechaActualizacion { get; set; } = DateTime.Now;
+        public int Estado { get; set; } = 1;
+
+
+
+    }
+}
